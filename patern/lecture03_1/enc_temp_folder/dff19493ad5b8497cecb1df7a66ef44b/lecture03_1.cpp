1af50e1424ab103830af63a242bd220f0e767d5e
Wsy <www9216@naver.com>
2024-03-20 11:21:53 +0900

    [game loop]

--- patern/lecture03_1/enc_temp_folder/dff19493ad5b8497cecb1df7a66ef44b/lecture03_1.cpp
+++ patern/lecture03_1/enc_temp_folder/dff19493ad5b8497cecb1df7a66ef44b/lecture03_1.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main()
 {
 	int num = 0;
-	string name;
 	cout << "게임이 실행됐습니다" << endl; /*endl = 엔터 */
 	while (num != 9999)
 	{
 		cout << "내가 싫어하는 숫자를 써봐 : " << endl;
-		cin >> name >> num;
-		cout << name << "왜그러니... 그 숫자가 아니야" << endl;
+		cin >> num;
+		cout << "왜그러니... 그 숫자가 아니야" << endl;
 	}
 	cout << "잘했어"<< num<<"이라는 숫자를 좋아하는구나 " << endl;
 	return 0;
