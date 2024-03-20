a09d4ff00c5229b774f239990578509863362b3e
Wsy <www9216@naver.com>
2024-03-20 11:17:41 +0900

    [c++ game loop]

---
+++ patern/lecture03/lecture03/lecture03.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+/* stream = 연결통로 */
+int main()
+{
+	
+	int num;
+	cout << "난 C++이 싫다";
+	cin >> num;
+	cout << "근데 C++을 " << num << "번 때려쳐도 안풀릴거같다.";
+	return 0;
+}
---
+++ patern/lecture03_1/lecture03_1/lecture03_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+/* stream = 연결통로 */
+int main()
+{
+	int num = 0;
+	cout << "게임이 실행됐습니다" << endl; /*endl = 엔터 */
+	while (num != 9999)
+	{
+		cout << "내가 싫어하는 숫자를 써봐 : " << endl;
+		cin >> num;
+		cout << "왜그러니... 그 숫자가 아니야" << endl;
+	}
+	cout << "잘했어"<< num<<"이라는 숫자를 좋아하는구나 " << endl;
+	return 0;
+}
