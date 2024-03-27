d80e53a984422ebe8ec13b178cf9d11857018de8
damsister <hzzang08@gmail.com>
2024-03-27 11:08:44 +0900

    [lec4]

---
+++ patern/0320Project/0320Project/0320Project.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+//using namespace std;
+
+int main()
+{	
+	int num = 0;
+	std::string name;
+	std::cout << "이지호 : 아 힘들다..." << std::endl;
+
+	while (num !=18)
+	{
+		std::cout << "나쁜놈의 이름과 나이를 입력하세요...";
+		std::cin >> name >> num;
+	}
+	
+	std::cout << "이지호: 유한상 " << num << "유한상..." << name << "..." << std::endl;
+	
+	
+	return 0;
+}
+
---
+++ patern/lecture04/lecture04/lecture04.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#pragma comment(lib, "Opengl32.lib")
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	//glfw라이브러리 초기화
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1280, 768, "WSYEngine", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetErrorCallback();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+		
+	}
+
+	glfwTerminate();
+	return 0;
+}
