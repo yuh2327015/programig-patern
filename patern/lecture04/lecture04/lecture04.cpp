0ce0b7e9cac3a667ec080808875e8387c739dfa4
damsister <hzzang08@gmail.com>
2024-03-27 11:23:30 +0900

    [lec04]

--- patern/lecture04/lecture04/lecture04.cpp
+++ patern/lecture04/lecture04/lecture04.cpp
@@ -2,6 +2,17 @@
 #pragma comment(lib, "Opengl32.lib")
 #include <GLFW/glfw3.h>
 
+void errorCallback(int error, const char* description)
+{
+	std::cerr << "GLFW Error: " << description << std::endl;
+}
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mo)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
 int main(void)
 {
 	//glfw라이브러리 초기화
@@ -17,12 +28,13 @@ int main(void)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetErrorCallback();
+	glfwSetErrorCallback(errorCallback);
+	glfwSetKeyCallback(window, keyCallback);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
-
+		glClearColor(1.0f,0.0f,1.0f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		glfwSwapBuffers(window);
