0eb1f1f42bd085578daa8e5a3f9edac8dd5ca654
yuh2327015 <www9216@naver.com>
2024-04-03 13:51:35 +0900

    [hw]

---
+++ HW/Hw05/Lecture05-HW/Lecture05-HW.cpp
@@ -0,0 +1,101 @@
+﻿#pragma comment(lib, "Opengl32.lib")
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+float moveFactor = 0.0f;
+float scaleFactor = 1.0f;
+
+void errorCallback(int error, const char* description)
+{
+	std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		moveFactor += 0.01f;
+	}
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+	{
+		scaleFactor += 0.1f;
+	}
+}
+
+int render()
+{
+	glBegin(GL_TRIANGLES);
+	//점1
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f * scaleFactor, (0.0f + moveFactor) * scaleFactor);
+	//점2
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.5f * scaleFactor, (0.5f + moveFactor) * scaleFactor);
+	//점3
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-0.5f * scaleFactor, (0.5f + moveFactor) * scaleFactor);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f * scaleFactor, (1.0f + moveFactor) * scaleFactor);
+	//점2
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.25f * scaleFactor, (0.25f + moveFactor) * scaleFactor);
+	//점3
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-0.5f * scaleFactor, (-0.5f + moveFactor) * scaleFactor);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.15f * scaleFactor, (0.5f + moveFactor) * scaleFactor);
+	//점2
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-0.5f * scaleFactor, (0.5f + moveFactor) * scaleFactor);
+	//점3
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.5f * scaleFactor, (-0.5f + moveFactor) * scaleFactor);
+
+
+	glEnd();
+
+	return 0;
+}
+
+
+int main(void)
+{
+	//glfw라이브러리 초기화
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSetErrorCallback(errorCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		render();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
