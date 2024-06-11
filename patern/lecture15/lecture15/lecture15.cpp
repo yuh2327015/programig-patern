fbc9b1761d6a0f07c92664687489d3bc68d65fc4
yuh2327015 <www9216@naver.com>
2024-06-11 10:41:55 +0900

    [lec15]

---
+++ patern/lecture15/lecture15/lecture15.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void Eventhandler()
+{
+    cout << "끄아아아악" << endl;
+}
+
+int main()
+{
+
+    void (*onCollisionEnter)();
+    
+    onCollisionEnter = Eventhandler;
+    onCollisionEnter();
+    
+}
